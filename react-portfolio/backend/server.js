8145562a61b8a5b1336e784bd8d50c5a622e48ab
Annie <95302202+AnniePriya@users.noreply.github.com>
2023-11-25 20:50:59 +0530

    Update server.js

--- react-portfolio/backend/server.js
+++ react-portfolio/backend/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3001;
+const port = 3000;
 
 // Serve static files from the build folder of your React app
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '')));
 
 // Define your API routes
 app.get('/api/about', (req, res) => {
@@ -21,7 +21,7 @@ app.get('/api/about', (req, res) => {
 
 // Send the React app as the default route
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname,  'index.html'));
 });
 
 app.listen(port, () => {
